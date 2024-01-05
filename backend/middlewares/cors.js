635d4f6648a843a1c748e0cf0ea7142102cf8207
Andrey Ognev <enquence@yandex.ru>
2024-01-05 23:16:47 +0300

    fix: CORS adding header first

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,14 +12,15 @@ module.exports = function (req, res, next) {
   const { method } = req;
   const { origin } = req.headers;
 
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.end();
   }
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
   next();
 };
