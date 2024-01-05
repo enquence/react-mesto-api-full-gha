c3eeaf9f69b4bdfdaa92f9dca09d97d9d5654b9d
Andrey Ognev <enquence@yandex.ru>
2024-01-05 23:04:46 +0300

    fix: trying tests #1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -63,7 +63,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true }).status(StatusCodes.OK).end();
+      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true }).status(StatusCodes.OK).send({ token });
     })
     .catch(next);
 };
--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -6,8 +6,8 @@ const allowedCors = [
   'localhost:3000',
 ];
 
-module.exports = (req, res, next) => {
-  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+module.exports = function (req, res, next) {
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
   const { method } = req;
   const { origin } = req.headers;
@@ -15,11 +15,11 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
   next();
-}
+};
